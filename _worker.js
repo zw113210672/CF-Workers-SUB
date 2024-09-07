3dc28b071b2bac83bef5e519a3d0a49505ae19b1
zw113210672 <134625594+zw113210672@users.noreply.github.com>
2024-09-07 22:19:20 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -14,11 +14,11 @@ let cacheTTL = 24 ;//小时，缓存时长
 
 //节点链接 + 订阅链接
 let MainData = `
-vless://b7a392e2-4ef0-4496-90bc-1c37bb234904@cf.090227.xyz:443?encryption=none&security=tls&sni=edgetunnel-2z2.pages.dev&fp=random&type=ws&host=edgetunnel-2z2.pages.dev&path=%2F%3Fed%3D2048#%E5%8A%A0%E5%85%A5%E6%88%91%E7%9A%84%E9%A2%91%E9%81%93t.me%2FCMLiussss%E8%A7%A3%E9%94%81%E6%9B%B4%E5%A4%9A%E4%BC%98%E9%80%89%E8%8A%82%E7%82%B9
-https://sub.xf.free.hr/auto
+vless://9ca4fd33-7b81-4c7b-8518-bdfc413b5f09@104.19.57.160:2086?encryption=none&security=none&sni=dalin622.wei11310672.workers.dev&fp=randomized&type=ws&host=dalin622.wei11310672.workers.dev&path=%2F%3Fed%3D2048#dalin622.wei11310672.workers.dev
 `
 
-let urls = [];
+
+let urls = [https://worker-yellow-hill-20ab.wei11310672.workers.dev/sub/104.18.137.113];
 let subconverter = "SUBAPI.fxxk.dedyn.io"; //在线订阅转换后端，目前使用CM的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
 let subconfig = "https://raw.githubusercontent.com/cmliu/ACL4SSR/main/Clash/config/ACL4SSR_Online_MultiCountry.ini"; //订阅配置文件
 let subProtocol = 'https';
